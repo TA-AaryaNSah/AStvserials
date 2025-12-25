ffb5905c8458d6ffeb7d7a173c3e4f99740a3d22
Aaryan Sah <167690903+TA-AaryaNSah@users.noreply.github.com>
2025-12-26 00:53:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 
 PICS = (environ.get('PICS', 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglY7112nyBzRAwFub45XA_jL2n9LBzxQomHHi3gZgJzQ-sHpSf58bK2OyPZZgyfAb3OvpOSdmoDlpyVTLzc07O5h_yF-VWAckjU4Fa-WVjvBcpvvc-5eecRjivGhud2-I1vMs-SdEMKv0SSKRPdpmp6B15hIqYrhRoEin6nOTStzZyuymcH-JlJz8bF2Ni/s1600/20251225_204908.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
+MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglY7112nyBzRAwFub45XA_jL2n9LBzxQomHHi3gZgJzQ-sHpSf58bK2OyPZZgyfAb3OvpOSdmoDlpyVTLzc07O5h_yF-VWAckjU4Fa-WVjvBcpvvc-5eecRjivGhud2-I1vMs-SdEMKv0SSKRPdpmp6B15hIqYrhRoEin6nOTStzZyuymcH-JlJz8bF2Ni/s1600/20251225_204908.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://img.odivex.com/img/img_1757259312_155545e6ba.jpg'))
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
